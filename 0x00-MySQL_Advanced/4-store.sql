e08b38b2a6607edebd0c5af21575afdd8d3848b1
TeddyMuli <tmuli974@gmail.com>
2024-10-20 21:44:00 +0300

    4-store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,30 +1,4 @@
--- Create the items table
+-- An sql script that triggers recalculations if an order is placed
 
-CREATE TABLE IF NOT EXISTS items (
-  item_id INT AUTO_INCREMENT PRIMARY KEY,
-  item_name VARCHAR(255) NOT NULL,
-  quantity INT NOT NULL
-);
-
--- Create the orders table
-
-CREATE TABLE IF NOT EXISTS orders (
-  order_id INT AUTO_INCREMENT PRIMARY KEY,
-  item_id INT NOT NULL,
-  order_quantity INT NOT NULL,
-  FOREIGN KEY (item_id) REFERENCES items(item_id)
-);
-
--- Create the trigger to decrease quantity after adding a new order
-DELIMITER //
-
-CREATE TRIGGER decrease_quantity_after_order
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.order_quantity
-  WHERE item_id = NEW.item_id;
-END //
-
-DELIMITER ;
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON ORDERS FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
