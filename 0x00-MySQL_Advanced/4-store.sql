55acc0463fa2d6e4038fb043347258af31df57b9
TeddyMuli <tmuli974@gmail.com>
2024-10-20 22:03:21 +0300

    4-store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,4 @@
 -- An sql script that triggers recalculations if an order is placed
 
-CREATE TRIGGER decrease_items_quantity
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.number
-  WHERE name=NEW.item_name;
-END;
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
