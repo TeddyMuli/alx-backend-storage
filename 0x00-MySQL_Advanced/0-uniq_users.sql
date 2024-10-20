3a03952709034105147719a499ded7b098a49a25
TeddyMuli <tmuli974@gmail.com>
2024-10-20 21:29:16 +0300

    add comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,7 @@
+-- Create Users table
+
 CREATE TABLE IF NOT EXISTS users (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email varchar(255) NOT NULL UNIQUE,
   name varchar(255)
-)
+);
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,8 @@
+-- Create users table with a country field
+
 CREATE TABLE IF NOT EXISTS users (
   id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email varchar(255) NOT NULL UNIQUE,
   name varchar(255),
   country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-)
+);
